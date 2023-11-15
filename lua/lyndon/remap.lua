7c60546aa279e72fad83c93d8cbe01cf0a16de46
Lyndon <lyndon.armitage@gmail.com>
2023-11-15 18:39:17 +0000

    Add some remaps for yanking to clipboard

--- lua/lyndon/remap.lua
+++ lua/lyndon/remap.lua
@@ -4,3 +4,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Yank everything to clipboard
 vim.keymap.set("n", "<leader>ya", ":%y+<CR>")
+
+-- Yank to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
