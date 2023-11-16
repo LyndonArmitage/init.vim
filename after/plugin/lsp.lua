741ad2ae5a0d276d21b6f57aced5c2c955467254
Lyndon <lyndon.armitage@gmail.com>
2023-11-16 10:44:03 +0000

    Enable a bunch of LSP modules
    Still not configured proper key mappings but will do

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,5 +1,54 @@
 local lsp_zero = require('lsp-zero')
 
+-- Dockerfile
+-- npm install -g dockerfile-language-server-nodejs
+require'lspconfig'.dockerls.setup{}
+
+-- Bash
+-- npm i -g bash-language-server
+require'lspconfig'.bashls.setup{}
+
+-- C
+-- Installed with package
+require'lspconfig'.ccls.setup{}
+
+-- Zig
+-- Installed with package
+require'lspconfig'.zls.setup{}
+
+-- Lua
+-- Installed with package
+require'lspconfig'.lua_ls.setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME
+              -- "${3rd}/luv/library"
+              -- "${3rd}/busted/library",
+            }
+            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+            -- library = vim.api.nvim_get_runtime_file("", true)
+          }
+        }
+      })
+
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+    return true
+  end
+}
+
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
