32f6a8f2f2d9181741156981c01d0fcc25b6dbd0
Lyndon <lyndon.armitage@gmail.com>
2023-11-17 18:32:20 +0000

    Add concrete bindings for lsp zero

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,10 +3,42 @@ local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  lsp_zero.default_keymaps({ buffer = bufnr })
+  --lsp_zero.default_keymaps({ buffer = bufnr })
 
-  local opts = { buffer = bufnr, remap = false }
+  local opts = { buffer = bufnr, remap = true }
+  -- It's time to try out some keybinds, I will follow the defaults and maybe
+  -- add my own similar to IntelliJ
+
+  -- Ctrl+P shows signature help
   vim.keymap.set("n", "<C-P>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+  -- <F18> is Actually Shift+<F6>
+  -- So this matches IntelliJs refactor/rename
+  vim.keymap.set("n", "<F18>", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "<S-F6>", vim.lsp.buf.rename, opts)
+
+  -- Reformat buffer
+  vim.keymap.set("n", "<F3>", vim.lsp.buf.format, opts)
+
+  -- Go to Definition
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  -- Go to Declaration (not always supported)
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+  -- Go to type def
+  vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, opts)
+  -- Go to implementation
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+
+  -- List references, pretty good!
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+
+  -- Diagnostics stuff
+  vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+  
+
 end)
 
 -- Dockerfile
