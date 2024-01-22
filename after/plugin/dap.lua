0fd10fbd32aac42cdb2a2e467424ba30a1bf4fa9
Lyndon <lyndon.armitage@gmail.com>
2024-01-22 21:29:55 +0000

    Move dap commands to own file & add c support

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -18,3 +18,44 @@ dap.configurations.scala = {
     },
   },
 }
+
+dap.configurations.c = {
+  {
+    name = "Launch",
+    type = "gdb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = "${workspaceFolder}",
+  },
+}
+
+-- DAP keybinds based on: https://github.com/scalameta/nvim-metals/discussions/39
+vim.keymap.set("n", "<leader>dc", function()
+  dap.continue()
+end)
+
+vim.keymap.set("n", "<leader>dr", function()
+  dap.repl.toggle()
+end)
+
+vim.keymap.set("n", "<leader>dK", function()
+  require("dap.ui.widgets").hover()
+end)
+
+vim.keymap.set("n", "<leader>dt", function()
+  dap.toggle_breakpoint()
+end)
+
+vim.keymap.set("n", "<leader>dso", function()
+  dap.step_over()
+end)
+
+vim.keymap.set("n", "<leader>dsi", function()
+  dap.step_into()
+end)
+
+vim.keymap.set("n", "<leader>dl", function()
+  dap.run_last()
+end)
--- after/plugin/metals.lua
+++ after/plugin/metals.lua
@@ -15,35 +15,6 @@ vim.api.nvim_create_autocmd("FileType", {
       vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end)
       vim.keymap.set("n", "K", vim.lsp.buf.hover)
       vim.keymap.set("n", "<leader>t", function() require("telescope").extensions.metals.commands() end)
-
-      -- DAP keybinds based on: https://github.com/scalameta/nvim-metals/discussions/39
-      vim.keymap.set("n", "<leader>dc", function()
-        require("dap").continue()
-      end)
-
-      vim.keymap.set("n", "<leader>dr", function()
-        require("dap").repl.toggle()
-      end)
-
-      vim.keymap.set("n", "<leader>dK", function()
-        require("dap.ui.widgets").hover()
-      end)
-
-      vim.keymap.set("n", "<leader>dt", function()
-        require("dap").toggle_breakpoint()
-      end)
-
-      vim.keymap.set("n", "<leader>dso", function()
-        require("dap").step_over()
-      end)
-
-      vim.keymap.set("n", "<leader>dsi", function()
-        require("dap").step_into()
-      end)
-
-      vim.keymap.set("n", "<leader>dl", function()
-        require("dap").run_last()
-      end)
     end
     require("metals").initialize_or_attach(metals_config)
   end,
