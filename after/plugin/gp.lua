5bde7bae6a14c83de8dd1d25b096260bc17af25e
Lyndon <lyndon.armitage@gmail.com>
2024-06-05 09:28:38 +0100

    Add ChatGPT command shortcuts

--- after/plugin/gp.lua
+++ after/plugin/gp.lua
@@ -3,3 +3,19 @@ local conf = {
 }
 
 require("gp").setup(conf)
+
+-- ChatGPT Bindings
+-- https://github.com/robitx/gp.nvim?tab=readme-ov-file#native
+
+local function keymapOptions(desc)
+  return {
+    noremap = true,
+    silent = true,
+    nowait = true,
+    desc = "GPT prompt " .. desc,
+  }
+end
+
+vim.keymap.set({ "n", "i" }, "<C-g>f", "<cmd>GpChatFinder<cr>", keymapOptions("Chat Finder"))
+vim.keymap.set({ "n", "i" }, "<C-g>c", "<cmd>GpChatNew<cr>", keymapOptions("New Chat"))
+vim.keymap.set({ "n", "i" }, "<C-g>t", "<cmd>GpChatToggle<cr>", keymapOptions("Toggle Chat"))
