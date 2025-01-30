c8313d0774fdc1162f14c4fb3ac990a304f935ff
Lyndon <lyndon.armitage@gmail.com>
2025-01-30 14:57:48 +0000

    Move ChatGPT config to plugin location & make lazy

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -169,7 +169,75 @@ return {
 
   -- ChatGPT? At this time of year? At this time of day? In this operating
   -- system? Localised within your editor?
-  { "robitx/gp.nvim" },
+  {
+    "robitx/gp.nvim",
+    opts = function()
+      -- Get Key from an invoked command
+      local config_dir = vim.fn.stdpath('config')
+      return {
+        openai_api_key = { "bash", config_dir .. "/get-openai-api.sh" }
+      }
+    end,
+    keys = {
+      {
+        "<C-g>f",
+        "<cmd>GpChatFinder<cr>",
+        desc = "GPT Chat Finder",
+        noremap = true,
+        silent = true,
+        nowait = true,
+      },
+      {
+        "<C-g>c",
+        "<cmd>GpChatNew<cr>",
+        desc = "GPT New Chat",
+        noremap = true,
+        silent = true,
+        nowait = true,
+      },
+      {
+        "<C-g>t",
+        "<cmd>GpChatToggle<cr>",
+        desc = "GPT Toggle Chat",
+        noremap = true,
+        silent = true,
+        nowait = true,
+      },
+    },
+    cmd = {
+      "GpChatNew",
+      "GpChatPaste",
+      "GpChatToggle",
+      "GpChatFinder",
+      "GpChatRespond",
+      "GpChatDelete",
+      "GpRewrite",
+      "GpAppend",
+      "GpPrepend",
+      "GpEnew",
+      "GpNew",
+      "GpVnew",
+      "GpTabnew",
+      "GpPopup",
+      "GpImplement",
+      "GpContext",
+      "GpWhisper",
+      "GpWhisperRewrite",
+      "GpWhisperAppend",
+      "GpWhisperPrepend",
+      "GpWhisperEnew",
+      "GpWhisperNew",
+      "GpWhisperVnew",
+      "GpWhisperTabnew",
+      "GpWhisperPopup",
+      "GpNextAgent",
+      "GpAgent",
+      "GpImage",
+      "GpImageAgent",
+      "GpStop",
+      "GpInspectPlugin",
+    },
+  },
 
   -- Heritage simply makes sure any parent directories exist when writing a new
   -- file.
@@ -177,7 +245,7 @@ return {
 
   -- PlantUML Syntax is not supported by Tree Sitter
   { "aklt/plantuml-syntax" },
-  { "tyru/open-browser.vim",   lazy = true },
+  { "tyru/open-browser.vim",  lazy = true },
   {
     "weirongxu/plantuml-previewer.vim",
     dependencies = {
