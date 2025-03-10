287fc3b58ac41f7446e718427f315ae3e7e48d9b
Lyndon <lyndon.armitage@gmail.com>
2025-03-10 15:57:19 +0000

    Add harper tool for writing, works like Grammarly

--- after/plugin/writing.lua
+++ after/plugin/writing.lua
@@ -27,3 +27,38 @@ vim.api.nvim_create_user_command(
   end,
   { nargs = 0 }
 )
+
+-- A writing LSP
+-- https://writewithharper.com/docs/integrations/neovim
+-- Can install with pacman
+-- sudo pacman -S harper
+require('lspconfig').harper_ls.setup {
+  filetypes = { "markdown", "gitcommit", "html", "mail", "plaintext" },
+  settings = {
+    ["harper-ls"] = {
+      userDictPath = "",
+      fileDictPath = "",
+      linters = {
+        SpellCheck = false,
+        SpelledNumbers = false,
+        AnA = true,
+        SentenceCapitalization = true,
+        UnclosedQuotes = true,
+        WrongQuotes = false,
+        LongSentences = true,
+        RepeatedWords = true,
+        Spaces = true,
+        Matcher = true,
+        CorrectNumberSuffix = true
+      },
+      codeActions = {
+        ForceStable = false
+      },
+      markdown = {
+        IgnoreLinkTitle = false
+      },
+      diagnosticSeverity = "hint",
+      isolateEnglish = false
+    }
+  }
+}
