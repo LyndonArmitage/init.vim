0172a81321f19c88a090cb21257a575e319d8c5d
Lyndon <lyndon.armitage@gmail.com>
2025-06-09 15:14:35 +0100

    Add neovide config if needed

---
+++ after/plugin/gui.lua
@@ -0,0 +1,9 @@
+-- Config in case running neovim in some kind of GUI
+
+if vim.g.neovide then
+  vim.o.guifont = "DejaVu Sans Mono:h12"
+  -- turn off the annoying cursor animation
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_short_animation_length = 0
+  vim.g.neovide_cursor_vfx_mode = ""
+end
