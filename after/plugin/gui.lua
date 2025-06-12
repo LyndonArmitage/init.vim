90bb85a0252d07a2d51b93038f3b9d91ef9c942d
Lyndon <lyndon.armitage@gmail.com>
2025-06-12 16:08:06 +0100

    Alter GUI font to be a Nerd font

--- after/plugin/gui.lua
+++ after/plugin/gui.lua
@@ -1,7 +1,7 @@
 -- Config in case running neovim in some kind of GUI
 
 if vim.g.neovide then
-  vim.o.guifont = "DejaVu Sans Mono:h12"
+  vim.o.guifont = "DejaVuSansM Nerd Font Mono:h12"
   -- turn off the annoying cursor animation
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_cursor_short_animation_length = 0
