a3f8b23d10ff92234c38a0cb163bbcf4cb049f0a
Lyndon <lyndon.armitage@gmail.com>
2025-06-16 11:04:59 +0100

    Tidy up LSP double binds and add fidget.nvim
    fidget.nvim will show me messages from the LSPs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -98,13 +98,10 @@ return {
       metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
       metals_config.settings.testUserInterface = "Test Explorer"
       metals_config.settings.serverVersion = "latest.snapshot" -- Maybe remove?
-      metals_config.init_options.statusBarProvider = "on"
+      metals_config.init_options.statusBarProvider = "off"
       metals_config.init_options.testExplorerProvider = true
       metals_config.on_attach = function(client, bufnr)
         require("metals").setup_dap()
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, { desc = "Go to next diagnostic" })
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, { desc = "Go to previous diagnostic" })
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "LSP Hover" })
         vim.keymap.set("n", "<leader>t", function() require("telescope").extensions.metals.commands() end,
           { desc = "Toggle Metals Telescope" })
         vim.keymap.set("n", "<leader>Tr", function() require("metals.tvp").toggle_tree_view() end,
@@ -122,6 +119,12 @@ return {
         end
       })
     end
+  },
+
+  -- Basic info for LSP messages etc
+    {
+    "j-hui/fidget.nvim",
+    opts = {},
   },
 
   -- Debugging support
