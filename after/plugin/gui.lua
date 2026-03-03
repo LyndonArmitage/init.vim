1dd5e8e0fc45cabfe316ad786853a203d381d516
Lyndon <lyndon.armitage@gmail.com>
2026-03-03 15:06:15 +0000

    Add CTRL+SHIFT+V to paste in neovide

--- after/plugin/gui.lua
+++ after/plugin/gui.lua
@@ -6,4 +6,9 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_cursor_short_animation_length = 0
   vim.g.neovide_cursor_vfx_mode = ""
+
+  -- Add CTRL+SHIFT+V as a paste command from system clipboard/register
+  vim.keymap.set({ "n", "v", "i", "c" }, "<C-S-v>", function()
+    vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
+  end, { noremap = true, silent = true })
 end
